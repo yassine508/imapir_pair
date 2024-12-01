a43980cf3f41e259cf55db3f2bbe5a1a2b521c05
yassine508 <chemlaliyassine77@gmail.com>
2024-12-01 13:42:18 +0100

    impair et pair

---
+++ script.js
@@ -0,0 +1,11 @@
+function impair_pair(i){
+        for (var i=1; i<= 10;i++){
+            if(i%2==0){
+                console.log(i+ " pair")
+            }
+            else{
+                console.log( i+" impair")
+            }
+        }
+    }
+    impair_pair()
